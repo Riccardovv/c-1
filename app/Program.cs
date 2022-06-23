76b2d8b4f8ac5e2b40f347af9ccc3193c20300ae
Ricardo <ricardovalverde16@gmail.com>
2022-06-23 17:56:12 +0100

    1

---
+++ app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleAPP
+{
+    class Program
+    {
+        static void Main(string [] args){
+            Console.WriteLine("works");
+            int num1= 5;
+            int num2= 6;
+            double num3= 2.57;
+            double suma= num1 + num3;
+
+            Console.WriteLine(suma);
+            string valor1= Console.ReadLine();
+            Console.WriteLine("El valor introduciod es: " + valor1);
+            int valorInt= Convert.ToInt32(valor1);
+            Console.WriteLine("valor int" + valorInt);
+        }
+    }
+}
